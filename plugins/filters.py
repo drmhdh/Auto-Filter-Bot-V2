04cd19e6d86f4141bfe1febd9be7a0efeabd5077
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-03-11 22:08:24 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -168,7 +168,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("HELP", callback_data="help_data"),
                     InlineKeyboardButton("ABOUT", callback_data="about_data")],
-                [InlineKeyboardButton("⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")]
+                [InlineKeyboardButton("📚  JOIN OUR GROUP 🔬", url="https://t.me/dent_tech_for_u")]
             ])
 
             await query.message.edit_text(
@@ -183,7 +183,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("BACK", callback_data="start_data"),
                     InlineKeyboardButton("ABOUT", callback_data="about_data")],
-                [InlineKeyboardButton("⭕️ SUPPORT ⭕️", url="https://t.me/TroJanzSupport")]
+                [InlineKeyboardButton("📚  SUPPORT 📚 ", url="https://t.me/dent_tech_for_books")]
             ])
 
             await query.message.edit_text(
@@ -198,7 +198,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("BACK", callback_data="help_data"),
                     InlineKeyboardButton("START", callback_data="start_data")],
-                [InlineKeyboardButton("SOURCE CODE", url="https://github.com/TroJanzHEX/Auto-Filter-Bot-V2")]
+                [InlineKeyboardButton("CASE STUDY", url="https://t.me/dental_case_study")]
             ])
 
             await query.message.edit_text(
